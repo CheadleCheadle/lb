fa27812fc92290f2f4d0e31e364ea4d2a55d1f0a
CheadleCheadle <gcheadle123@gmail.com>
2023-10-16 13:42:46 -0700

    added proxy request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -10,9 +11,29 @@ app.get('/', (req, res) => {
         `Host: ${req.hostname}\n`,
         `User-Agent: ${req.originalUrl}\n`,
     );
-    res.send("Hi Mom!");
+
+    const req_options = {
+        hostname: 'localhost',
+        port: 8000,
+        path: '/',
+        method: 'GET',
+    };
+
+    const proxy_request = http.request(req_options, (proxy_res) => {
+        res.writeHead(proxy_res.statusCode, proxy_res.headers);
+        proxy_res.pipe(res, {
+            end: true,
+        });
+    });
+
+    proxy_request.on('error', (err) => {
+        console.log(err);
+        res.status(500).send("Proxy request failed");
+    })
+
+    proxy_request.end();
 });
 
 app.listen(port, () => {
     console.log(`Listening in port ${port}`);
-})
+});
